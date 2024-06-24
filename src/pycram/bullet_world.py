8d3d86d7b286a3412d1f4caab509c97d0f329b8b
HaSu <hawkin@uni-bremen.de>
2024-06-24 17:10:51 +0200

    [minor] renaming node for working with multiple people

--- src/pycram/bullet_world.py
+++ src/pycram/bullet_world.py
@@ -60,7 +60,7 @@ class BulletWorld:
 
     # Check is for sphinx autoAPI to be able to work in a CI workflow
     if rosgraph.is_master_online():  # and "/pycram" not in rosnode.get_node_names():
-        rospy.init_node('pycram_vee')
+        rospy.init_node('pycram_hasu')
 
     def __init__(self, type: str = "GUI", is_shadow_world: bool = False):
         """
