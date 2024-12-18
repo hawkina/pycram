763c1213cb2a577466d93b266d1ea96ae45bb417
HaSu <hawkin@uni-bremen.de>
2024-12-18 19:04:15 +0100

    [neem-demo] setup demo loop

---
+++ demos/pycram_neem_demo/demo.py
@@ -0,0 +1,114 @@
+from pycram.worlds.bullet_world import BulletWorld
+from pycram.designators.action_designator import *
+from pycram.designators.location_designator import *
+from pycram.designators.object_designator import *
+from pycram.datastructures.enums import WorldMode
+from pycram.datastructures.pose import Pose
+from pycram.process_module import simulated_robot, with_simulated_robot
+from pycram.object_descriptors.urdf import ObjectDescription
+from pycram.world_concepts.world_object import Object
+from pycram.datastructures.dataclasses import Color
+from pycram.ros.viz_marker_publisher import VizMarkerPublisher
+from pycrap import Robot, Apartment, Milk, Cereal, Spoon, Bowl
+import numpy as np
+
+world = BulletWorld(WorldMode.GUI)
+extension = ObjectDescription.get_file_extension()
+robot = Object("pr2", Robot, f"pr2{extension}", pose=Pose([1, 2, 0]))
+apartment = Object("apartment", Apartment, f"apartment{extension}")
+robot_desig = BelieveObject(names=["pr2"])
+apartment_desig = BelieveObject(names=["apartment"])
+
+def setup_world():
+    global apartment
+    np.random.seed(420)
+    milk = Object("milk", Milk, "milk.stl", pose=Pose([2.5, 2, 1.02]), color=Color(1, 0, 0, 1))
+    cereal = Object("cereal", Cereal, "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]), color=Color(0, 1, 0, 1))
+    spoon = Object("spoon", Spoon, "spoon.stl", pose=Pose([2.4, 2.24, 0.85]), color=Color(0, 0, 1, 1))
+    bowl = Object("bowl", Bowl, "bowl.stl", pose=Pose([2.5, 2.2, 1.02]), color=Color(1, 1, 0, 1))
+    apartment.attach(spoon, 'cabinet10_drawer_top')
+
+
+
+
+
+@with_simulated_robot
+def move_and_detect(obj_type):
+    pick_pose = Pose([2.7, 2.15, 1])
+
+    NavigateAction(target_locations=[Pose([1.7, 2, 0])]).resolve().perform()
+
+    LookAtAction(targets=[pick_pose]).resolve().perform()
+
+    object_desig = DetectAction(technique=DetectionTechnique.TYPES, object_designator_description=BelieveObject(types=[obj_type])).resolve().perform()
+    return object_desig[0]
+
+
+def demo():
+    global world, robot, apartment_desig, robot_desig, apartment
+    with (simulated_robot):
+        ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+        MoveTorsoAction([0.25]).resolve().perform()
+
+        milk_desig = move_and_detect(Milk)
+
+        TransportAction(milk_desig,  [Pose([4.8, 3.55, 0.8])], [Arms.LEFT]).resolve().perform()
+
+        cereal_desig = move_and_detect(Cereal)
+
+        TransportAction(cereal_desig,  [Pose([5.2, 3.4, 0.8], [0, 0, 1, 1])],[Arms.RIGHT]).resolve().perform()
+
+        bowl_desig = move_and_detect(Bowl)
+
+        TransportAction(bowl_desig,  [Pose([5, 3.3, 0.8], [0, 0, 1, 1])], [Arms.LEFT]).resolve().perform()
+
+        # Finding and navigating to the drawer holding the spoon
+        handle_desig = ObjectPart(names=["handle_cab10_t"], part_of=apartment_desig.resolve())
+        drawer_open_loc = AccessingLocation(handle_desig=handle_desig.resolve(),
+                                            robot_desig=robot_desig.resolve()).resolve()
+
+        NavigateAction([drawer_open_loc.pose]).resolve().perform()
+
+        OpenAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
+        #spoon.detach(apartment)
+
+        # Detect and pickup the spoon
+        LookAtAction([apartment.get_link_pose("handle_cab10_t")]).resolve().perform()
+
+        spoon_desigs = DetectAction(technique=DetectionTechnique.TYPES,
+                                   object_designator_description=BelieveObject(types=[Spoon])).resolve().perform()
+        spoon_desig = spoon_desigs[0]
+        spoon = world.get_object_by_type(Spoon)[0]
+        spoon.detach(apartment)
+        #spoon_desig.detach(apartment)
+        pickup_arm = Arms.LEFT if drawer_open_loc.arms[0] == Arms.RIGHT else Arms.RIGHT
+        PickUpAction(spoon_desig, [pickup_arm], [Grasp.TOP]).resolve().perform()
+
+        ParkArmsAction([Arms.LEFT if pickup_arm == Arms.LEFT else Arms.RIGHT]).resolve().perform()
+
+        CloseAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
+
+        ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+        MoveTorsoAction([0.15]).resolve().perform()
+
+        # Find a pose to place the spoon, move and then place it
+        spoon_target_pose = Pose([4.85, 3.3, 0.8], [0, 0, 1, 1])
+        placing_loc = CostmapLocation(target=spoon_target_pose, reachable_for=robot_desig.resolve()).resolve()
+
+        NavigateAction([placing_loc.pose]).resolve().perform()
+
+        PlaceAction(spoon_desig, [spoon_target_pose], [pickup_arm]).resolve().perform()
+
+        ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+def reset():
+    global world, apartment
+    world.get_object_by_type(Milk)[0].set_pose(pose=Pose([2.5, 2, 1.02]))
+    world.get_object_by_type(Cereal)[0].set_pose(pose=Pose([2.5, 2.3, 1.05]))
+    spoon = world.get_object_by_type(Spoon)[0]
+    spoon.set_pose(pose=Pose([2.4, 2.24, 0.85]))
+    apartment.attach(spoon, 'cabinet10_drawer_top')
+    world.get_object_by_type(Bowl)[0].set_pose(pose=Pose([2.5, 2.2, 1.02]))
+
